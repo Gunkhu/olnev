e32f16d5926af8f3507d0259043097766d9ce3fb
Csp <csosaka21@gmail.com>
2026-03-31 13:16:35 +0200

    Kezdés

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module dev.tanto {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires transitive javafx.graphics;
+    opens dev.tanto to javafx.fxml;
+    exports dev.tanto;
+}
